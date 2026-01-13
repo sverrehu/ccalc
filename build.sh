f69cf1aed5411bb68b0c0b265a0e93227267f36b
Sverre H Huseby <shh@thathost.com>
2026-01-13 19:00:25 +0100

    feat: add initial regression tests

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
+#!/bin/sh
+
 gcc -O2 -std=c2x -o calc *.c -lm && strip calc
+if test "$?" = "0"
+then
+    CMD=./calc ./regression-test.sh
+fi
---
+++ regression-test.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+if test -z "${CMD}"
+then
+    CMD=./calc
+fi
+
+if test ! -x "${CMD}"
+then
+    echo "No executable '${CMD}' found. Build the program before running tests."
+    exit 1
+fi
+
+NUM_FAILED=0
+NUM_OK=0
+
+assert_equals() {
+    EXPECTED=$1
+    ACTUAL=$2
+    DESCRIPTION=$3
+    if test "${ACTUAL}" != "${EXPECTED}"
+    then
+        echo "${DESCRIPTION}: Expected '${EXPECTED}', got '${ACTUAL}'"
+        NUM_FAILED=$(expr ${NUM_FAILED} + 1)
+    else
+        NUM_OK=$(expr ${NUM_OK} + 1)
+    fi
+}
+
+test_exact() {
+    EXPECTED=$1
+    EXPRESSION=$2
+    ACTUAL="$("${CMD}" "${EXPRESSION}")"
+    assert_equals "${EXPECTED}" "${ACTUAL}" "${EXPRESSION}"
+}
+
+# operators
+test_exact "1" "1"
+test_exact "-1" "-1"
+test_exact "3" "1+2"
+test_exact "6" "1+2+3"
+test_exact "-1" "1-2"
+test_exact "-4" "1-2-3"
+test_exact "6" "2*3"
+test_exact "24" "2*3*4"
+test_exact "3" "9/3"
+test_exact "1" "9/3/3"
+test_exact "7" "1+2*3"
+test_exact "7" "1+(2*3)"
+test_exact "12" "(1+3)*3"
+test_exact "1" "10%3"
+test_exact "0" "9%3"
+test_exact "8" "2^3"
+test_exact "262144" "4^(3^2)"
+test_exact "262144" "4^3^2"
+test_exact "4096" "(4^3)^2"
+
+# functions
+# the use of "round(1000* ... )" is for coping with rounding errors
+test_exact "0" "sin(0)"
+test_exact "1" "sin(pi/2)"
+test_exact "1" "cos(0)"
+test_exact "0" "round(10000*cos(pi/2))"
+
+if test "${NUM_FAILED}" = "0"
+then
+    echo "All ${NUM_OK} tests OK"
+else
+    echo "Failed tests: ${NUM_FAILED} (ok tests: ${NUM_OK})"
+    exit 1
+fi
